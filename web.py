a2d286a26daefd0925b7367a86a6e19c5d1684f7
李奇昱 <liqiyu@liqiyudeMacBook-Pro.local>
2026-01-30 21:05:12 +0800

    Change default port to 8000 for Railway compatibility

--- web.py
+++ web.py
@@ -34,6 +34,7 @@ def api_query():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
+    # Railway 会自动设置 PORT，如果没有则默认 8000
+    port = int(os.environ.get('PORT', 8000))
     debug = os.environ.get('FLASK_ENV') == 'development'
     app.run(host='0.0.0.0', port=port, debug=debug)
